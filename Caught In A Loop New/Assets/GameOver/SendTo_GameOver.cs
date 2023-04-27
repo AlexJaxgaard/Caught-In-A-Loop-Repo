96644aa23e311a84f9a524c39962d2e18b1098b4
Alexander Jaxgård <snalexdci@hotmail.com>
2023-04-27 16:02:25 +0200

    Updated the game

--- Caught In A Loop New/Assets/GameOver/SendTo_GameOver.cs
+++ Caught In A Loop New/Assets/GameOver/SendTo_GameOver.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 
 public class SendTo_GameOver : MonoBehaviour
 {
-        public Text m_MyText;
+    private VideoPlayer videoPlayer;
+    public Text m_MyText;
         public string Text_To_Write;
+
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
 
     public void LoadScene()
     {
         m_MyText.text = Text_To_Write;
         SceneManager.LoadScene("GameOver");
+    }
+
+    private void Update()
+    {
+        if (videoPlayer.isPaused)
+        {
+            LoadScene();
+        }
     }
 }
