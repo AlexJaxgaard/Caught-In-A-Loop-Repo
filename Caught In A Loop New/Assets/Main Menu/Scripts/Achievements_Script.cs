a6dde928150b05c83c8b7b42ae041a7c911b339d
maol6362 <maol6362@student.su.se>
2023-05-23 12:10:20 +0200

    Started with AchievementsButton

---
+++ Caught In A Loop New/Assets/Main Menu/Scripts/Achievements_Script.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievements_Script : MonoBehaviour
+{
+    private string GetAchievementStatus()
+    {
+        
+        return null;
+    }
+    
+    public void AchievementViewer()
+    {
+
+    }
+}
