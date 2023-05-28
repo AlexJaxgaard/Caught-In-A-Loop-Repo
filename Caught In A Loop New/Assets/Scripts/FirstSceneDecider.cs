8bad0fb8d7e9cbde11e1e2c55aee467a2bc2b640
Alexander Jaxgård <snalexdci@hotmail.com>
2023-05-28 21:26:55 +0200

    First Scene prologue

---
+++ Caught In A Loop New/Assets/Scripts/FirstSceneDecider.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstSceneDecider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("FirstSceneLoaded") || PlayerPrefs.GetString("FirstSceneLoaded") == "true")
+        {
+            PlayerPrefs.SetString("FirstSceneLoaded", "true");
+            loadScene("MainMenu");
+        } else if (!PlayerPrefs.HasKey("FirstSceneLoaded") || PlayerPrefs.GetString("FirstSceneLoaded") == "false")
+        {
+            PlayerPrefs.SetString("FirstSceneLoaded", "true");
+            loadScene("Prolog_Intro");
+
+    }
+
+        void loadScene(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
